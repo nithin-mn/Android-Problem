0a2585323e0e6e7639b313fc6710af10367ee5bb
Nithin <nithin.mn@surya-soft.com>
2021-04-06 11:42:10 +0530

    Solve Problem

---
+++ Problem/app/src/main/java/com/example/android/problem/ServiceBuilder.kt
@@ -0,0 +1,21 @@
+package com.example.android.problem
+
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ServiceBuilder {
+
+    private val client = OkHttpClient.Builder().build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("http://surya-interview.appspot.com/") 
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(client)
+        .build()
+
+    fun<T> buildService(service: Class<T>): T{
+        return retrofit.create(service)
+    }
+}
