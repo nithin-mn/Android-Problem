1c303e6da79b37edf3a173fd8bc1aef0d411ec8a
Nithin <nithin.mn@surya-soft.com>
2021-04-08 15:37:54 +0530

    Modify Files

--- AndroidProblem/app/src/main/java/com/example/android/androidproblem/CustomAdapter.kt
+++ AndroidProblem/app/src/main/java/com/example/android/androidproblem/CustomAdapter.kt
@@ -1,6 +1,8 @@
 package com.example.android.androidproblem
 
-import android.app.Activity
+
+
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,29 +14,29 @@ import coil.load
 import coil.request.ImageRequest
 
 class CustomAdapter(
-    private val context: Activity,
-    private val description: ArrayList<String>,
-    private val imageurl: ArrayList<String>
-) : ArrayAdapter<String>(context, R.layout.user_item, description) {
+    private val contex: Context,
+    private val description: List<User>,
+) : ArrayAdapter<User>(contex, R.layout.user_item ,description) {
 
     override fun getView(position: Int, view: View?, parent: ViewGroup): View {
-        val inflater: LayoutInflater = context.layoutInflater
+        val inflater=LayoutInflater.from(contex)
         val rowView = inflater.inflate(R.layout.user_item, null, true)
 
         val userDetail: TextView = rowView.findViewById(R.id.user_detail)
         val userImage: ImageView = rowView.findViewById(R.id.user_image)
-        userDetail.text = description[position]
+        val str="${description[position].firstName} ${description[position].lastName}\n${description[position].emailId}"
+        userDetail.text= str
 
-        val request = ImageRequest.Builder(context)
-            .data(imageurl[position])
+        val request = ImageRequest.Builder(contex)
+            .data(description[position].imageUrl)
             .target(
-                onStart = { placeholder ->
-                    userImage.load(placeholder)
+                onStart = {
+                    userImage.load(R.drawable.default_image)
                 },
                 onSuccess = { result ->
                     userImage.setImageDrawable(result)
                 },
-                onError = { error ->
+                onError = {
                     userImage.load(R.drawable.default_image)
                 }
             )
--- AndroidProblem/app/src/main/java/com/example/android/androidproblem/UserInfo.kt
+++ AndroidProblem/app/src/main/java/com/example/android/androidproblem/UserInfo.kt
@@ -5,14 +5,10 @@ import com.google.gson.annotations.SerializedName
 data class UserRequest(val emailId: String)
 
 data class User(
-        @SerializedName("emailId") val emailId: String,
-        @SerializedName("imageUrl") val imageUrl: String,
-        @SerializedName("firstName") val firstName: String,
-        @SerializedName("lastName") val lastName: String
-){
-        override fun toString(): String {
-                return "$firstName $lastName\n$emailId\n"
-        }
-}
+    @SerializedName("emailId") val emailId: String,
+    @SerializedName("imageUrl") val imageUrl: String,
+    @SerializedName("firstName") val firstName: String,
+    @SerializedName("lastName") val lastName: String
+)
 
 data class UserList(@SerializedName("items") val items: List<User>)
